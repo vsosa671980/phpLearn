4f9abd10e56211e69845ebc054ece0dc554ee579
Vicente Sosa <vsosaal@gmail.com>
2022-06-25 21:24:55 +0200

    first new

---
+++ src/funciones.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funciones comunes</title>
+</head>
+<body>
+
+<?php
+
+//Funciones comumes de PHP
+
+$nombre = "Vicente Sosa";
+
+var_dump($nombre);
+
+
+
+?>
+    
+</body>
+</html>
+
+
+
